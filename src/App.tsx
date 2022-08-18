fb702cfa360e3375d96808c2f02e925576d53249
Bence Darabos <makosbab@gmail.com>
2022-08-18 11:46:53 +0200

    first geolocation test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+//navigator.geolocation.watchPosition((data)=>{console.log("Update", data)}, (err)=>{console.log("Err", err)}, {timeout: 5000, maximumAge: 0, enableHighAccuracy: false});
+
+const App = () => {
+  const [speed, setSpeed] = useState(0);
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 500,
+    maximumAge: 0  
+  };
+
+  const successCb = (pos:any) => {
+    console.log("succ", pos.coords.speed);
+    setSpeed(pos.coords.speed);
+  }
+  const errorCb = (err:any) => {
+    console.log("err", err);
+  }
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(successCb, errorCb, options);
+    }
+
+    // if ('geolocation' in navigator) {
+    //   navigator.geolocation.watchPosition((data)=>{console.log("Update", data)}, (err)=>{console.log("Err", err)}, {timeout: 5000, maximumAge: 0, enableHighAccuracy: false});
+
+    //   navigator.geolocation.getCurrentPosition((data) => {
+    //     console.log(data);
+    //     setLocation("");
+    //   });
+    // } else {
+    //   /* geolocation IS NOT available */
+    // }
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{Number(speed)}</h1>
     </div>
   );
 }
