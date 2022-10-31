7b6ec07fa1a3e302845089a231e4d4a58cc0a420
Bence Darabos <makosbab@gmail.com>
2022-10-31 12:56:31 +0100

    service worker add for PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,3 +11,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Update PWA on reload if internet is available?
+serviceWorkerRegistration.register();
